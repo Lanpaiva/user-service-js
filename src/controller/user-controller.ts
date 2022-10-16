3f5c1e9169f98858eb927f2692a40af08e4b5f3e
Alan Paiva <allanpaiva21@outlook.com>
2022-10-16 01:28:01 -0300

    fix: adjust files

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -1,15 +1,15 @@
 import { Request, Response, Router } from 'express';
 
 export class UserController {
-  private _router = Router()
+  private _router = Router();
 
   public get router() {
-    return this._router
+    return this._router;
   }
 
   constructor() {
     this._router.get('/', (_: Request, res: Response) => {
-      res.send('Hello from my Controller!!!')
-    })
+      res.send('Hello from my Controller!!!');
+    });
   }
 }
